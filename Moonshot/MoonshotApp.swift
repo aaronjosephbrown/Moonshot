83dead006b2e03f3198e02ee2a69fe8163ebfc97
Aaron J. Brown <aaron.joseph.brown@me.com>
2023-10-01 19:43:42 -0500

    Initial Commit

---
+++ Moonshot/MoonshotApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoonshotApp.swift
+//  Moonshot
+//
+//  Created by Aaron Brown on 10/1/23.
+//
+
+import SwiftUI
+
+@main
+struct MoonshotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
